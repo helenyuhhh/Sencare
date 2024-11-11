139111b180e3a83c9a9fccf62cfa1b2cbb96f3e9
helenyuhhh <helenyu301@gmail.com>
2024-11-10 20:00:40 -0500

    tests list is created

--- Screens/PatientDetailsScreen.js
+++ Screens/PatientDetailsScreen.js
@@ -1,6 +1,6 @@
 // from now I want thisbutton only shows the basic details as an internatiate page
 // it will show patient's everything instead of clinical data cause it is stored in tests
-import { Text ,View, StyleSheet, Button } from "react-native";
+import { Text ,View, StyleSheet, Button,Image } from "react-native";
 // future plan: add a button to the record data screen ti update the latest data?
 
 const PatientDetailsScreen = ({route, navigation}) => {
@@ -11,19 +11,21 @@ const PatientDetailsScreen = ({route, navigation}) => {
     }
     return (
         <View style = {styles.viewStyle}>
+            <Image style={styles.imageStyle} source={{
+                    uri: patient.picture
+                }}></Image>
             <Text style={styles.textStyle}> {"Patient Name: "+ patient.name.first +" "} {patient.name.last} </Text>
+            <Text style={styles.textStyle}> { "Room Number: " + patient.room} </Text>
             <Text style={styles.textStyle}> { "Age: " + patient.age} </Text>
             <Text style={styles.textStyle}> { "Gender: " + patient.gender} </Text>
-            <Text style={styles.textStyle}> { "Test: " + patient.tests.category} </Text>
+            
             {/* patient tests is defined as an json array, i want to view it through some button */}
-            <Text style={styles.textStyle}> { "Systolic: " + patient.clinical.systolic} </Text>
-            <Text style={styles.textStyle}> { "Diastolic: " + patient.clinical.diastolic} </Text>
-            <Text style={styles.textStyle}> { "Condition: " + patient.clinical.condition } </Text>
+            
             <Text style={styles.textStyle}> { "Weight: " + patient.weight} </Text>
             <Text style={styles.textStyle}> { "Height: " + patient.height} </Text>
-            <Text style={styles.textStyle}> { "Recorded Date: " +patient.date} </Text>
+            
             {/*  */}
-            <Button title="View or add clinical data" onPress={()=>{
+            <Button title="View Tests list" onPress={()=>{
                 // this acts like sender, sent the patient to next screen, var is called toPatientTest
             navigation.navigate('PatientTests', {toPatientTest:patient})
         }}></Button>
@@ -42,11 +44,17 @@ const PatientDetailsScreen = ({route, navigation}) => {
 
 const styles = StyleSheet.create({
     viewStyle:{
-        alignItems: "stretch"
+        alignItems: "stretch",
+        //justifyContent: "center"
     },
     textStyle : {
         padding:5,
         fontSize: 25
+    },
+    imageStyle: {
+        width: 100,
+        height: 100,
+        alignSelf:"center"
     }
 })
 
--- Screens/PatientTestScreen.js
+++ Screens/PatientTestScreen.js
@@ -1,18 +1,48 @@
 // from now I want thisbutton only shows the basic details as an internatiate page
 // it will show patient's everything instead of clinical data cause it is stored in tests
-import { Text ,View, StyleSheet, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text ,View, StyleSheet, Button, FlatList, TouchableOpacity} from "react-native";
 // future plan: add a button to the record data screen ti update the latest data?
 
 const PatientTestScreen = ({route, navigation}) => {
+    const [tests,setTestsList] = useState([])
     // act like the receiver, receives the patient passes from last screen. receices as toPatientTest
     const patient = route.params.toPatientTest
     // TO DO: list of patients tests, just liek patient lists
+    // try to fetch the tests from patient
+    const fetchTests = async() => {
+        fetch(`http://172.16.7.126:3000/api/patients/${patient._id}`).
+            then(response => response.json()).then(data => {
+                setTestsList(data.tests)
+            } )
+    } 
+    useEffect(() => { 
+        fetchTests()
+    },[])
+    testRow = (test) => 
+        <TouchableOpacity onPress={()=>{
+            props.navigation.navigate('TestDetail', {toTestDetail:test})
+        }}>
+            <View style={styles.viewStyle}>
+                <Text style={styles.textStyle}>{test.category}</Text>
+                <Text style={styles.dateStyle}>{test.date}</Text>
+              
+        </View>
+
+        </TouchableOpacity>
+
     return (
-        <View style = {styles.viewStyle}>
+        <View>
             
            
             {/* patient tests is defined as an json array, i want to view it through some button */}
-            <Text style={styles.textStyle}> { "test: " + patient.tests} </Text>
+            <FlatList
+                data={tests}
+                 keyExtractor={(item,i) => i}
+                renderItem={ (listItem) => 
+                    testRow(listItem.item)
+                }
+            ></FlatList>
             
             {/*  */}
             
@@ -30,12 +60,23 @@ const PatientTestScreen = ({route, navigation}) => {
 */
 
 const styles = StyleSheet.create({
-    viewStyle:{
-        alignItems: "stretch"
+    viewStyle: {
+        flexDirection: "column",
+        alignItems: 'stretch',
+        justifyContent:'space-between',
+        borderWidth: 1,
+        margin: 15,
+        borderRadius: 5,
+        padding: 10
     },
     textStyle : {
         padding:5,
-        fontSize: 25
+        fontSize: 20,
+        fontWeight:"bold"
+    },
+    dateStyle:{
+        padding:5,
+        fontSize: 15
     }
 })
 
